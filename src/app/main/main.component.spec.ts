d309a60c9e18e7b09da309b965f8bda44c204850
Kunal Ojha <kunalojha12@gmail.com>
2020-09-02 22:00:19 -0500

    swapping component names for better semantics

--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -2,15 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainComponent } from './main.component';
 
-describe('MainComponent', () => {
+describe('IntroComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [MainComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
